f82193ccff0e0b9110a57bd42f41d3392e254e19
HoangVietSE <viethoangtien@gmail.com>
2020-01-02 13:24:56 +0700

    Modify customview

--- app/src/main/java/com/beetech/tienichmuasam/custom/QuantitySelectedCustomView.java
+++ app/src/main/java/com/beetech/tienichmuasam/custom/QuantitySelectedCustomView.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
